0bfbe8ebce3fcee339e785ccb7106fd4a89aa14d
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-06 12:27:17 -0400

    Manipulação de arquivos

---
+++ File.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+//CRIAR fs.writeFile("test.txt", "Olá NodeJs", (err) => console.log(err));
+//ADICIONAR  appendFile === colocar mais coisas no arquivo
+//RENOMEAR fs.rename("test.txt", "test2.txt", (err) => console.log(err));
+//REMOVER fs.unlink("test2.txt", (err) => console.log(err));
+
+//MOSTRAR CAMINHO console.log(__dirname);
