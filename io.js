c22077f9376d27f865cf8a1843c80755954fbb17
Aldenir Dos Santos <aldenirtakeshi@gmail.com>
2022-12-06 12:15:45 -0400

    IO process

--- io.js
+++ io.js
@@ -1,3 +1,8 @@
 const process = require("process");
 
-console.log(process.argv);
+// console.log(process.argv);
+// process.stdout.write("Qual é o seu nome ? ");
+process.stdin.on("data", (keyboard) => {
+  process.stdout.write(`Texto do Usário: ${keyboard}`);
+  process.exit();
+});
